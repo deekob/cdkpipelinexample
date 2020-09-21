7a214bcaacca80bf88ba55346dbbc309dc8afd69
Derek Bingham <derek.w.bingham@gmail.com>
2020-09-21 21:12:31 +0800

    fixing paths

--- bin/cdkpipeline-demo.ts
+++ bin/cdkpipeline-demo.ts
@@ -2,11 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { PipeLineStack } from '../lib/pipeline-stack';
-
 const app = new cdk.App();
-new PipeLineStack (app, 'PipeLineStack',
-{
-    env: {account: '978928857807', region: 'ap-southeast-2'},
-});
+ new PipeLineStack (app, 'PipeLineStack',
+ {
+     env: {account: '978928857807', region: 'ap-southeast-2'},
+ });
+
 
 app.synth();
