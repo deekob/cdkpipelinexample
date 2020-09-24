8b04ea445fb41fb9935ccedabdbe465642a7edf6
Derek Bingham <derek.w.bingham@gmail.com>
2020-09-24 12:38:19 +0800

    added message

--- lambda/hello.js
+++ lambda/hello.js
@@ -3,6 +3,6 @@ exports.handler = async function(event) {
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
-      body: `Hello and Welcome to the CDK!\n`
+      body: `Hello - I think you have just CDK'd your first pipeline :)!\n`
     };
   };
