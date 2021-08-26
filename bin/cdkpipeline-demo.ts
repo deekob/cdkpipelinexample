0ed369c95bcbf7c4a87cd6dacca93e71577c63aa
Derek Bingham <derek.w.bingham@gmail.com>
2021-08-26 13:27:33 +0800

    updated with new versions

--- bin/cdkpipeline-demo.ts
+++ bin/cdkpipeline-demo.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { PipeLineStack } from '../lib/pipeline-stack';
+import { MyPipelineStack} from '../lib/pipeline-stack';
 const app = new cdk.App();
- new PipeLineStack (app, 'PipeLineStack',
+ new MyPipelineStack (app, 'PipeLineStack',
  {
      env: {account: '978928857807', region: 'ap-southeast-2'},
  });
