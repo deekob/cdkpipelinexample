c98b6bcae46b58c8668b62e5491ac2705fb32a3a
Derek Bingham <derek.w.bingham@gmail.com>
2021-08-26 14:01:53 +0800

    fixing name conflicts

--- bin/cdkpipeline-demo.ts
+++ bin/cdkpipeline-demo.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MyPipelineStack} from '../lib/pipeline-stack';
+import { HWNMyPipelineStack} from '../lib/pipeline-stack';
 const app = new cdk.App();
- new MyPipelineStack (app, 'PipeLineStack',
+ new MyPipelineStack (app, 'HWNPipeLineStack',
  {
      env: {account: '978928857807', region: 'ap-southeast-1'},
  });
